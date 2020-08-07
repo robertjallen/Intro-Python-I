a82a06422aed837530b05f0384203095354498cb
robertjallen <bobbidigi34@gmail.com>
2020-08-07 19:06:05 -0400

    add fil.io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+foo = open('foo.txt')
+print(foo.read())
+foo.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+bar = open('bar.txt', 'w+')
+bar.write('Like winds from an hour glass.\nthese,\nare the days of our lives.')
+bar.close()
+bar = open('bar.txt', 'r')
+print(bar.read())
